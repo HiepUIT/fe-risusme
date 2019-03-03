3f4524b07b94f56ddf6ac8d09f5636d782a12605
Tran Quang Hiep <tranquanghiep@Mr-Tran.local>
2019-03-03 21:26:56 +0700

    Updates

---
+++ precache-manifest.1fc213d9922fbc4238168d858ec11825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1d3d0308a3b42361f2",
+    "url": "/fe-risusme/static/js/runtime~main.1a1d3d03.js"
+  },
+  {
+    "revision": "59d024d127bae81cecfa",
+    "url": "/fe-risusme/static/js/main.59d024d1.chunk.js"
+  },
+  {
+    "revision": "e11f6d2e12a2d2c520aa",
+    "url": "/fe-risusme/static/js/2.e11f6d2e.chunk.js"
+  },
+  {
+    "revision": "59d024d127bae81cecfa",
+    "url": "/fe-risusme/static/css/main.5316ab84.chunk.css"
+  },
+  {
+    "revision": "5c514f17eac87b9d8cd83e67c8cd7019",
+    "url": "/fe-risusme/index.html"
+  }
+];
